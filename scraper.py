7d3cc0d16daf4754ae7c1fb46fad519b06d6e9dd
Anton <antonbogatirev1@gmail.com>
2022-11-21 18:54:40 -0800

    Scraping multiple pages and sleep delay
    Adding functionality to scrape multiple pages.
    Adding sleep delay between scrapes.

--- scraper.py
+++ scraper.py
@@ -1,16 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 
 all_quotes = []
-res = requests.get("http://quotes.toscrape.com")
-soup = BeautifulSoup(res.text, "html.parser")
-quotes = soup.find_all(class_="quote")
+url = "http://quotes.toscrape.com"
+page = "/page/1/"
 
-for quote in quotes:
-    all_quotes.append({
-        "text": quote.find(class_="text").get_text(),
-        "author": quote.find(class_="author").get_text(),
-        "bio-link": quote.find("a")["href"] 
-    })
+while page:
+    res = requests.get(f"{url}{page}")
+    print(f"Now Scraping {url}{page}...")
+    soup = BeautifulSoup(res.text, "html.parser")
+    quotes = soup.find_all(class_="quote")
+
+    for quote in quotes:
+        all_quotes.append({
+            "text": quote.find(class_="text").get_text(),
+            "author": quote.find(class_="author").get_text(),
+            "bio-link": quote.find("a")["href"] 
+        })
+    next_btn = soup.find(class_="next")
+    page = next_btn.find("a")["href"] if next_btn else None
+    sleep(2)
 
 print(all_quotes)
