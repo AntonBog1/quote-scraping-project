b891c24bd7712b5e47809e938b48c0a5b3eb1f88
Anton <antonbogatirev1@gmail.com>
2022-11-23 09:00:51 -0800

    Adding game functionality

--- scraper.py
+++ scraper.py
@@ -1,25 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
 
-all_quotes = []
-url = "http://quotes.toscrape.com"
-page = "/page/1/"
+URL = "http://quotes.toscrape.com"
 
-while page:
-    res = requests.get(f"{url}{page}")
-    print(f"Now Scraping {url}{page}...")
-    soup = BeautifulSoup(res.text, "html.parser")
-    quotes = soup.find_all(class_="quote")
+def scrape_quotes():
+    all_quotes = []
+    page = "/page/1/"
+    while page:
+        res = requests.get(f"{URL}{page}")
+        print(f"Now Scraping {URL}{page}...")
+        soup = BeautifulSoup(res.text, "html.parser")
+        quotes = soup.find_all(class_="quote")
 
-    for quote in quotes:
-        all_quotes.append({
-            "text": quote.find(class_="text").get_text(),
-            "author": quote.find(class_="author").get_text(),
-            "bio-link": quote.find("a")["href"] 
-        })
-    next_btn = soup.find(class_="next")
-    page = next_btn.find("a")["href"] if next_btn else None
-    sleep(2)
+        for quote in quotes:
+            all_quotes.append({
+                "text": quote.find(class_="text").get_text(),
+                "author": quote.find(class_="author").get_text(),
+                "bio-link": quote.find("a")["href"] 
+            })
+        next_btn = soup.find(class_="next")
+        page = next_btn.find("a")["href"] if next_btn else None
+        sleep(2)
+    return all_quotes
 
-print(all_quotes)
+def start_game(quotes):
+    quote = choice(quotes)
+    remaining_guesses = 4
+    print("Here's a quote: ")
+    print(quote["text"])
+    guess = ""
+    while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+        guess = input(f"Who said this quote? Guesses remaining: {remaining_guesses}\n")
+        if guess.lower() == quote["author"].lower():
+            print("YOU GOT IT RIGHT!")
+            break
+        remaining_guesses -= 1
+        if remaining_guesses == 3:
+            res = requests.get(f"{URL}{quote['bio-link']}")
+            soup = BeautifulSoup(res.text, "html.parser")
+            birth_date = soup.find(class_="author-born-date").get_text()
+            birth_place = soup.find(class_="author-born-location").get_text()
+            print(f"Here's a hint: The author was born on {birth_date} {birth_place}")
+        elif remaining_guesses == 2:
+            print(f"Here's a hint: The author's first name starts with: {quote['author'][0]}")
+        elif remaining_guesses == 1:
+            last_initial = quote['author'].split(" ")[1][0]
+            print(f"Here's a hint: The author's last name starts with {last_initial}")
+        else: 
+            print(f"Sorry you ran out of guesses. The answer was {quote['author']}")
+
+    again = ""
+    while again.lower() not in ('y', 'yes', 'n', 'no'):
+        again = input("Would you like to play again?\n")
+    if again.lower() in ('yes', 'y'):
+        return start_game(quotes)
+    else:
+        print("Ok, goodbye!")
+    
+quotes = scrape_quotes()
+start_game(quotes)
